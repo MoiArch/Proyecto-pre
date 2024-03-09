7b7859e48cb3d0517b066fdf5c69a9ad86ebd103
MoiArch <you@example.com>
2024-03-09 10:10:29 -0600

    v6.0

--- RentACarrito/Domain/Customers/Customer.cs
+++ RentACarrito/Domain/Customers/Customer.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using Domain.Primitives;
 using Domain.ValueObjects;
 
@@ -6,41 +5,36 @@ namespace Domain.Customers;
 
 public sealed class Customer : AggregateRoot
 {
-    private CustomerId customerId;
-    private PhoneNumber phoneNumber;
-    private Address address;
-    private bool v;
-    private CustomerId customerId1;
-    private PhoneNumber phoneNumber1;
-    private Address address1;
-
-    public Customer(CustomerId customerId1, string name, string lastName, string email, DuiNumber duiNumber, PhoneNumber phoneNumber1, Address address1, bool v)
+    public Customer(CustomerId id, string name, string lastName, string email, DuiNumber duiNumber,
+    PhoneNumber phoneNumber, Address address, bool active)
     {
-        this.customerId1 = customerId1;
+        Id = id;
         Name = name;
         LastName = lastName;
         Email = email;
         DuiNumber = duiNumber;
-        this.phoneNumber1 = phoneNumber1;
-        this.address1 = address1;
-        this.v = v;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        Active = active;
     }
 
     private Customer()
     {
+
     }
 
     public CustomerId Id { get; private set; }
     public string Name { get; private set; } = string.Empty;
-    public string LastName { get; private set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public string FullName => $"{Name} {LastName}";
     public string Email { get; private set; } = string.Empty;
-    public DuiNumber DuiNumber { get; private set; }
+    public DuiNumber DuiNumber{ get; private set; }
     public PhoneNumber PhoneNumber { get; private set; }
     public Address Address { get; private set; }
-    public bool Active {get; private set;}
+    public bool Active { get; private set; }
+
     public static Customer UpdateCustomer(Guid id, string name, string lastName, string email,DuiNumber duiNumber, PhoneNumber phoneNumber, Address address, bool active)
     {
-        return new Customer(new CustomerId(id), name, lastName, email, duiNumber, phoneNumber, address, active);
+        return new Customer(new CustomerId(id), name, lastName, email,duiNumber, phoneNumber, address, active);
     }
 }
